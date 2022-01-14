d19710b35de9255c843b976e6f53eab74b16488e
Alexis-dev508 <alexisjesus773@gmail.com>
2022-01-14 13:26:21 -0600

    Se creo el index

--- src/firebase/main_firebase.js
+++ src/firebase/main_firebase.js
@@ -1,22 +1,44 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBJDeYRxEIPvDI15Kcuq0ZSfeVszMvjnsA",
-  authDomain: "proyecto-8voidgs.firebaseapp.com",
-  projectId: "proyecto-8voidgs",
-  storageBucket: "proyecto-8voidgs.appspot.com",
-  messagingSenderId: "165371869769",
-  appId: "1:165371869769:web:0b8261c46d72ccfb4bdd98",
-  measurementId: "G-7YHKVSH59S"
-};
+firebase.initializeApp({
+    apiKey: "AIzaSyBJDeYRxEIPvDI15Kcuq0ZSfeVszMvjnsA",
+    authDomain: "proyecto-8voidgs.firebaseapp.com",
+    projectId: "proyecto-8voidgs",
+    storageBucket: "proyecto-8voidgs.appspot.com",
+    messagingSenderId: "165371869769",
+    appId: "1:165371869769:web:0b8261c46d72ccfb4bdd98",
+    measurementId: "G-7YHKVSH59S"
+});
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+var db = firebase.firestore();
+
+function addRestaurant(){
+    try {
+        let resName = document.getElementById('res_name').value;
+        let resOwner = document.getElementById('res_owner').value
+        let resSchedule = document.getElementById('res_schedule').value;
+        let resDescription = document.getElementById('res_description').value;
+        let resType = document.getElementById('res_type').value;
+        let resPhoto = document.getElementById('res_photo');
+        
+        let restaurantRef = db.collection("restaurant");
+        restaurantRef.doc(resName).set({
+            description: resDescription,
+            menu: '',
+            name: resName,
+            orders: '',
+            owner: resOwner,
+            schedule: resSchedule,
+            status: 'abierto',
+            type: resType,
+            value: '60' 
+        }).then(()=>{
+            console.log("Document written with ID: ", docRef.id);
+        });   
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
 
